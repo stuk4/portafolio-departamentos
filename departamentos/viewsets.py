9f82b3d9dcfb40ef393dd3bab1a1c34a918befb8
KyleCepeda <duostorm666@gmail.com>
2020-11-13 22:48:30 -0300

    agrego api inventario

--- departamentos/viewsets.py
+++ departamentos/viewsets.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
 from rest_framework import serializers
-from .models import Departamento
-from .serializer import DepartamentoSerializer
+from rest_framework import generics
+from .models import Departamento, Inventario
+from .serializer import DepartamentoSerializer, InventarioSerializer
 
-class DepartamentoViewSet(viewsets.ModelViewSet):
+class DepartamentoMantencionViewSet(viewsets.ModelViewSet):
     queryset = Departamento.objects.exclude(usuario__isnull=False).filter(estado_mantencion=True)
     serializer_class = DepartamentoSerializer
+
+class DepartamentosDisponiblesViewSet(viewsets.ModelViewSet):
+    queryset = Departamento.objects.exclude(usuario__isnull=False).filter(estado_mantencion=False)
+    serializer_class = DepartamentoSerializer
+
+class InventarioViewset(viewsets.ModelViewSet):
+    queryset = Inventario.objects.all()
+    serializer_class = InventarioSerializer
